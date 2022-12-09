e188011d4d494f73874db53900d31f82e0d15ba6
ZhouSusan <szhou089@gmail.com>
2022-12-09 13:31:15 -0800

    Added Wizard (child class of Human) with constructor and FireBall() and Heal() in Wizard.cs

--- Human.cs
+++ Human.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Human
 {
-    internal class Human
+    public class Human
     {
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Dexterity { get; set; }
+        public int Health { get; set; }
+
+        public Human(string name)
+        {
+            this.Name = name;
+            this.Strength = 3;
+            this.Intelligence = 3;
+            this.Health = 3;
+            this.Health = 100;
+        }
+
+        public Human(string name, int streng, int intel, int dex, int health)
+        {
+            this.Name = name;
+            this.Strength = streng;
+            this.Intelligence = intel;
+            this.Dexterity = dex;
+            this.Health = health;
+        }
+
+        public virtual void Attack(Object obj)
+        {
+            Human enemy = (Human)obj;
+            if (enemy == null)
+            {
+                Console.WriteLine("Failed Attack");
+            }
+            else
+            {
+                enemy.Health -= 5 * this.Strength;
+            }
+        }
     }
 }
--- Wizard.cs
+++ Wizard.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Human
 {
-    internal class Class1
+    class Wizard : Human
     {
+        public Wizard(string name) : base (name)
+        {
+            this.Health = 50;
+            this.Intelligence = 25;
+        }
+
+        public int Heal(Wizard wiz)
+        {
+            wiz.Health += (10 * this.Intelligence);
+            return wiz.Health;
+        } 
+
+        public int Fireball(Object obj)
+        {
+            Random rand = new Random();
+            int dmg = rand.Next(20, 51);
+
+            Human humanEnemy = (Human)obj;
+            Wizard wizardEnemy = (Wizard)obj;
+
+            if(humanEnemy != null)
+            {
+                humanEnemy.Health -= dmg;
+                return humanEnemy.Health;
+            }
+
+            if (wizardEnemy != null)
+            {
+                wizardEnemy.Health -= dmg;
+                return wizardEnemy.Health;
+            }
+
+            return 0;
+        } 
     }
 }
